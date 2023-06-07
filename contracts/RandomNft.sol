74cc3ee49447d85b307e7f27e2bbebc149e133a9
Artemides <edmpulasky@gmail.com>
2023-06-06 20:16:18 -0500

    define the random nft requirements

---
+++ contracts/RandomNft.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.8;
+
+// users need to pay to mint an NFT
+// when minting a NFT request a chainlink VRF for a random number
+// get a super rare pub, sort of rare shina inu, St. bernard common
