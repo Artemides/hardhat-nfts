94e3b3ad74fa580fbafab42434d6154056072eff
Artemides <edmpulasky@gmail.com>
2023-06-06 20:26:04 -0500

    definde fulfill random words

--- contracts/RandomNft.sol
+++ contracts/RandomNft.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.8;
 // users need to pay to mint an NFT
 // when minting a NFT request a chainlink VRF for a random number
 // get a super rare pub, sort of rare shina inu, St. bernard common
+contract RandomNft {
+    constructor() {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+}
