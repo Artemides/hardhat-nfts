fbc4f0e7dcd3646a4e4b103764c6d51e953b08b0
Artemides <edmpulasky@gmail.com>
2023-06-07 17:41:12 -0500

    set up chainlink vrf coodinator

--- contracts/RandomNft.sol
+++ contracts/RandomNft.sol
@@ -4,8 +4,32 @@ pragma solidity ^0.8.8;
 // users need to pay to mint an NFT
 // when minting a NFT request a chainlink VRF for a random number
 // get a super rare pub, sort of rare shina inu, St. bernard common
-contract RandomNft {
-    constructor() {}
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract RandomNft is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private REQUEST_CONFIRMATIONS = 3;
+    uint32 private NUM_WORDS = 1;
+
+    constructor(
+        address vrfCoordinator,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestRandomNFT() public {}
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
 }
