d3e1197be7987a3144a144545e6b37abdea98f94
Artemides <edmpulasky@gmail.com>
2023-06-12 23:09:18 -0500

    deply random nft

---
+++ deploy/02-deploy-randomNft.ts
@@ -0,0 +1,48 @@
+import { ethers, network } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { developmentChains, networkConfig } from "../hardhat.config.helper";
+import { VRFCoordinatorV2Mock } from "../typechain-types";
+
+export const randomNFT = async (hre: HardhatRuntimeEnvironment) => {
+    const {
+        deployments: { deploy, log },
+        getNamedAccounts,
+    } = hre;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId ?? "";
+    let vrfCoordinatorV2Address: string;
+    let subscriptionId: string;
+    const gasLane = networkConfig[chainId].gasLane;
+    const callbackGasLimit = networkConfig[chainId].callbackGasLimit;
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock = await ethers.getContract(
+            "VRFCoordinatorV2Mock"
+        );
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+        const tx = await vrfCoordinatorV2Mock.createSubscription();
+        const { events } = await tx.wait(1);
+        subscriptionId = events ? events[0].args?.subId : "";
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2!;
+        subscriptionId = networkConfig[chainId].subscriptionId!;
+    }
+    const args = [vrfCoordinatorV2Address, subscriptionId, gasLane, callbackGasLimit];
+
+    await deploy("RandomNft", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: 1,
+    });
+
+    //get agregator
+    //deploy
+    //args []
+    //  vrfCoordinator,
+    //  subscriptionId,
+    //  gasLane,
+    //  callbackGasLimit,
+    //  heliosUris,
+    //  mintFee
+};
--- hardhat.config.helper.ts
+++ hardhat.config.helper.ts
@@ -1,1 +1,26 @@
+export type NetworkConfig = {
+    [key: string]: {
+        name: string;
+        vrfCoordinatorV2?: string;
+        gasLane: string;
+        subscriptionId?: string;
+        callbackGasLimit: string;
+    };
+};
+
+export const networkConfig: NetworkConfig = {
+    11155111: {
+        name: "sepolia",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        subscriptionId: "2400",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "500000",
+    },
+    31337: {
+        name: "local",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "500000",
+    },
+};
+
 export const developmentChains = ["local", "hardhat"];
