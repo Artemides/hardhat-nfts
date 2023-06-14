5309d5b6e70ec62ff954b20e14cec4ae8db3b8bc
Artemides <edmpulasky@gmail.com>
2023-06-14 13:30:44 -0500

    implement function to upload files to ipfs node

--- utils/uploadIPFS.ts
+++ utils/uploadIPFS.ts
@@ -1,5 +1,27 @@
-//path.resolve -- path to our files
-// readdir Sync
-// create a read strem of the file
+import pinataSDK, { PinataPinResponse } from "@pinata/sdk";
+import { resolve } from "path";
+import fs from "fs";
+import "dotenv/config";
 
-export function uploadFileToIPFS() {}
+const pinataApiKey = process.env.PINATA_API_KEY;
+const pinataApiSecret = process.env.PINATA_API_SECRET;
+const pinata = new pinataSDK(pinataApiKey, pinataApiSecret);
+
+export async function uploadFilesToIPFS(dirPath: string) {
+    const responses: PinataPinResponse[] = [];
+    const relativePath = resolve(dirPath);
+    const files = fs.readdirSync(relativePath);
+    console.log({ files });
+    console.log("Deploying files to pinata...");
+    for (const file in files) {
+        console.log(`Working on ${file}`);
+        try {
+            const readStream = fs.createReadStream(file);
+            const response = await pinata.pinFileToIPFS(readStream);
+            responses.push(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return { responses, files };
+}
