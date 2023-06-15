65d2f3203bfe599f92954f8e665181e7cec8cf14
Artemides <edmpulasky@gmail.com>
2023-06-14 23:00:43 -0500

    clean code

--- test/randomIpfsNft.test.ts
+++ test/randomIpfsNft.test.ts
@@ -1,21 +1,3 @@
-/* Constructor 
-1. Sets starting values correctly
-Request NFT
-1. fails if payment isn't sent with the request
-2. reverts if payment amount is less than the mint fee
-3. emits an event and kicks off a random word request
-
-fulfillRandomWords
-
-1. mints NFT after random number is returned
-
-getBreedFromModdedRng
-1. should return pug if moddedRng < 10
-2. should return shiba-inu if moddedRng is between 10 - 39
-3. should return st. bernard if moddedRng is between 40 - 99
-4. should revert if moddedRng > 9 
-*/
-
 import { deployments, ethers, network } from "hardhat";
 import { developmentChains, networkConfig } from "../hardhat.config.helper";
 import { RandomNft, VRFCoordinatorV2Mock } from "../typechain-types";
