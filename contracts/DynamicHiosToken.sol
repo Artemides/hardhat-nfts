f1ef65dfb010bc24ab7c85a31341d9b15229b177
Artemides <edmpulasky@gmail.com>
2023-06-15 23:53:16 -0500

    create file for svg token smark contract

---
+++ contracts/DynamicHiosToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Name {
+    // function to mint
+    // store our nfts
+    // logic to determine when to show x or y
+    //data:image/svg+xml;base65,....
+
+    constructor() {}
+}
