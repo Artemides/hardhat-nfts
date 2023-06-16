146afffc2003ee2842dc4d0dbd9c09d227f1da02
Artemides <edmpulasky@gmail.com>
2023-06-16 15:53:28 -0500

    verify

--- deploy/03-deploy-svgNft.ts
+++ deploy/03-deploy-svgNft.ts
@@ -2,6 +2,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { developmentChains, networkConfig } from "../hardhat.config.helper";
 import { ethers, network } from "hardhat";
 import fs from "fs";
+import { verify } from "../utils/verify";
 const svgHiosToken = async (hre: HardhatRuntimeEnvironment) => {
     const {
         getNamedAccounts,
@@ -24,14 +25,20 @@ const svgHiosToken = async (hre: HardhatRuntimeEnvironment) => {
 
     args = [pricefeedAddress, sadTokenSvg, happyTokenSvg];
 
-    await deploy("SvgHiosToken", {
+    const svgHiosToken = await deploy("SvgHiosToken", {
         from: deployer,
         args,
         log: true,
         waitConfirmations: 1,
     });
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("verifying svgHiosToken...");
+        await verify(svgHiosToken.address, args);
+        log("svgHiosToken verified");
+    }
 };
 
 export default svgHiosToken;
 
-svgHiosToken.tags = ["all", "mocks"];
+svgHiosToken.tags = ["all", "mocks", "svgNft"];
